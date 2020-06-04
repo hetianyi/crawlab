2aded1f83ea4ad925a76b1f8d6a5faec599c3ccc
hetianyi <hetianyi@hupu.com>
2020-06-04 15:38:48 +0800

    添加arm平台docker compose编排文件

--- frontend/src/utils/stats.js
+++ frontend/src/utils/stats.js
@@ -16,13 +16,13 @@ const sendEvCrawlab = async (eventCategory, eventAction, eventLabel) => {
 export default {
   sendPv (page) {
     if (localStorage.getItem('useStats') !== '0') {
-      window._hmt.push(['_trackPageview', page])
+      // window._hmt.push(['_trackPageview', page])
       sendEvCrawlab('访问页面', page, '')
     }
   },
   sendEv (category, eventName, optLabel, optValue) {
     if (localStorage.getItem('useStats') !== '0') {
-      window._hmt.push(['_trackEvent', category, eventName, optLabel, optValue])
+      // window._hmt.push(['_trackEvent', category, eventName, optLabel, optValue])
       sendEvCrawlab(category, eventName, optLabel)
     }
   }
